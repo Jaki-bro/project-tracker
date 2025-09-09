9610a8f706269ec2b3f698093c5a60c3e39769b8
Jaki-bro <zakiimada@gmail.com>
2025-09-09 08:17:08 +0700

    Fix db export + update ESLint

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined, check your .env.local file");
+  throw new Error("DATABASE_URL is not defined, check your .env file");
 }
 
-export const db = neon(process.env.DATABASE_URL);
+export const sql = neon(process.env.DATABASE_URL);
